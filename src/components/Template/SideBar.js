da2eefb613dae45877b15dfc685b6a3640afd61e
daredavil01 <sanket.tambare01@gmail.com>
2023-03-18 15:45:05 +0530

    PR-01: Updated code

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -26,17 +26,17 @@ const SideBar = () => (
       <p>
         Hi, I&apos;m Sanket. I like to build things. I am a softwware developer
         specialized in full stack development and data analysis. Currently I am
-        working as software developer at{' '}
+        working as software developer at{" "}
         <a href="https://www.linkedin.com/company/emtec-inc/mycompany/verification/">
           Emtec Inc
         </a>
         . Besides these I am interested in blogging, designing, video editing
-        and digital wellbeing. Checkout my{' '}
+        and digital wellbeing. Checkout my{" "}
         <a href="https://linktr.ee/daredavil">portfolio links</a>. Thank you!
       </p>
       <ul className="actions">
         <li>
-          {!window.location.pathname.includes('/resume') ? (
+          {!window.location.pathname.includes("/resume") ? (
             <Link to="/resume" className="button">
               Learn More
             </Link>
@@ -52,7 +52,7 @@ const SideBar = () => (
     <section id="footer">
       <ContactIcons />
       <p className="copyright">
-        &copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.
+        &copy; Sanket Tambare <Link to="/">daredavil01.com</Link>.
       </p>
     </section>
   </section>
