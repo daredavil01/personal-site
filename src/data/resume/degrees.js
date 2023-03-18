32f89115ba482e8837f762258faf72b96c15cf98
daredavil01 <sanket.tambare01@gmail.com>
2023-03-18 16:21:15 +0530

    PR-02: Updated code

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,8 +1,8 @@
 const degrees = [
   {
-    school: "RIT, Sangali",
-    degree: "B.Tech. Computer Science",
-    link: "https://ritindia.edu/",
+    school: 'RIT, Sangali',
+    degree: 'B.Tech. Computer Science',
+    link: 'https://ritindia.edu/',
     year: 2021,
   },
 ];
