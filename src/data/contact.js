1271e180c76409eec719375835a895161c3a0051
daredavil01 <sanket.tambare01@gmail.com>
2024-05-04 11:57:59 +0530

    Updated about page

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,41 +1,46 @@
-import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
-import { faSnowflake } from '@fortawesome/free-regular-svg-icons/faSnowflake';
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons/faEnvelope";
+import { faSnowflake } from "@fortawesome/free-regular-svg-icons/faSnowflake";
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
+// add code comment
+// data is variable that stores list of dictionary with keys: link, label, icon.
+// link is the url of the contact.
+// label is the text that will be displayed in the contact.
+// icon is the fontawesome icon that will be displayed in the contact.
+// example: {link: "https://github.com/daredavil01", label: "Github", icon: faGithub}
 const data = [
   {
-    link: 'https://linktr.ee/daredavil',
-    label: 'LinkTree',
+    link: "https://linktr.ee/daredavil",
+    label: "LinkTree",
     icon: faSnowflake,
   },
   {
-    link: 'https://github.com/daredavil01',
-    label: 'Github',
+    link: "https://github.com/daredavil01",
+    label: "Github",
     icon: faGithub,
   },
   {
-    link: 'https://www.instagram.com/sanket__239803/',
-    label: 'Instagram',
-    icon: faInstagram,
-  },
-  {
-    link: 'https://www.linkedin.com/in/sankettambare/',
-    label: 'LinkedIn',
+    link: "https://www.linkedin.com/in/sankettambare/",
+    label: "LinkedIn",
     icon: faLinkedinIn,
   },
   {
-    link: 'https://twitter.com/i_daredavil',
-    label: 'Twitter',
+    link: "https://twitter.com/i_daredavil",
+    label: "Twitter",
     icon: faTwitter,
   },
   {
-    link: 'mailto:sanket.tambare01@gmail.com',
-    label: 'Email',
+    link: "mailto:sanket.tambare01@gmail.com",
+    label: "Email",
+    icon: faEnvelope,
+  },
+  {
+    link: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    label: "XXXXXXXXXXXXX",
     icon: faEnvelope,
   },
 ];
