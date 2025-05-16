7338f815e4681ca6a90bc8182aef9041aece25ce
daredavil01 <sanket.tambare01@gmail.com>
2025-05-16 22:28:16 +0530

    Fixed responsive images

--- src/components/Instagram/ImageSlider.js
+++ src/components/Instagram/ImageSlider.js
@@ -4,30 +4,64 @@ import PropTypes from "prop-types";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
-const divStyle = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  backgroundSize: "cover",
-  height: "800px",
-};
+const ImageSlider = ({ data }) => {
+  const [dimensions, setDimensions] = React.useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
-const ImageSlider = ({ data }) => (
-  <article className="slide-container" style={{ width: "800px" }}>
-    <Slide>
-      {data.map((image) => (
-        <div key={image.caption}>
-          <div
-            style={{
-              ...divStyle,
-              backgroundImage: `url(${image.url})`,
-            }}
-          />
-        </div>
-      ))}
-    </Slide>
-  </article>
-);
+  React.useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const getResponsiveStyles = () => {
+    const isMobile = dimensions.width < 768;
+    return {
+      container: {
+        width: isMobile ? "100%" : "800px",
+        maxWidth: "100%",
+        margin: "0 auto",
+      },
+      slide: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: isMobile ? "300px" : "600px",
+        width: "100%",
+      },
+    };
+  };
+
+  const styles = getResponsiveStyles();
+
+  return (
+    <article className="slide-container" style={styles.container}>
+      <Slide autoplay={false} transitionDuration={500} indicators arrows>
+        {data.map((image) => (
+          <div key={image.caption}>
+            <div
+              style={{
+                ...styles.slide,
+                backgroundImage: `url(${image.url})`,
+              }}
+            />
+          </div>
+        ))}
+      </Slide>
+    </article>
+  );
+};
 
 ImageSlider.propTypes = {
   data: PropTypes.arrayOf(
