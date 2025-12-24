64fd037a533513e0c511f8a869be46dc52a07c77
daredavil01 <sanket.tambare01@gmail.com>
2025-12-24 11:26:04 +0530

    feat: add sports event data and corresponding images

--- src/data/sports.js
+++ src/data/sports.js
@@ -371,6 +371,38 @@ const sportsData = [
       }
     ],
   },
+  {
+    id: 15,
+    title: "Pune International Marathon 2025",
+    date: "December 7, 2025",
+    description: "My first Half Marathon as a pacer, target 2.15 Hrs!",
+    place: "Pune",
+    distance: "21 Kms",
+    time: "02:13:48",
+    timeCertificateLink: "https://www.racetecresults.com/myresults.aspx?CId=19898&RId=3340&EId=3&AId=610661",
+    slideImages: [
+      {
+        url: `${PUBLIC_URL}/images/sports/pim_1.jpeg`,
+        caption: "Slide 1",
+      },
+      {
+        url: `${PUBLIC_URL}/images/sports/pim_2.jpeg`,
+        caption: "Slide 2",
+      },
+      {
+        url: `${PUBLIC_URL}/images/sports/pim_3.jpeg`,
+        caption: "Slide 3",
+      },
+      {
+        url: `${PUBLIC_URL}/images/sports/pim_4.jpeg`,
+        caption: "Slide 4",
+      },
+      {
+        url: `${PUBLIC_URL}/images/sports/pim_5.jpeg`,
+        caption: "Slide 5",
+      },
+    ],
+  }
 ];
 
 export default sportsData;
