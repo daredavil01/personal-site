a8355ef4d81a357dd067d91c9a2ca0afe84f5609
daredavil01 <sanket.tambare01@gmail.com>
2026-01-15 23:30:07 +0530

    Added blogs list

--- src/data/100DaysToOffload.js
+++ src/data/100DaysToOffload.js
@@ -35,6 +35,24 @@ const data = [
     blog_date: '2026-01-08',
     blog_link: 'https://sankettambare.substack.com/p/the-choices-we-make',
   },
+  {
+    id: 5,
+    blog_title: 'Obsidian - The Enabler for Better Note-taking',
+    blog_description: 'Something to map your digital universe!',
+    challenge_id: '100_days_to_offload',
+    blog_tags: ['100_Days_to_Offload', 'Note-taking', 'Obsidian'],
+    blog_date: '2026-01-13',
+    blog_link: 'https://sankettambare.substack.com/p/obsidian-the-enabler-for-better-note',
+  },
+  {
+    id: 6,
+    blog_title: "Challenges: How I vibe-coded something to help me achieve do stuff!",
+    blog_description: "What it takes to balance that consistency and how easily can tech-enabled accountability boost your efforts!",
+    challenge_id: '100_days_to_offload',
+    blog_tags: ['100_Days_to_Offload', 'Challenges', 'Personal-Website'],
+    blog_date: '2026-01-15',
+    blog_link: 'https://sankettambare.substack.com/p/challenges-page',
+  }
 ];
 
 export default data;
