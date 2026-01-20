87801923e8cfc7d1153b7f59c4ac29edf2c6fef5
daredavil01 <sanket.tambare01@gmail.com>
2026-01-20 23:31:48 +0530

    Added TMM 2026 and blogs data

--- src/data/100DaysToOffload.js
+++ src/data/100DaysToOffload.js
@@ -52,6 +52,15 @@ const data = [
     blog_tags: ['100_Days_to_Offload', 'Challenges', 'Personal-Website'],
     blog_date: '2026-01-15',
     blog_link: 'https://sankettambare.substack.com/p/challenges-page',
+  },
+  {
+    id: 7,
+    blog_title: "What's really worth the efforts?",
+    blog_description: "The continuous running, reading, learning, emotional attachments, scrolling, efforts; what's something that matters the most!",
+    challenge_id: '100_days_to_offload',
+    blog_tags: ['100_Days_to_Offload', 'Self-reflection'],
+    blog_date: '2026-01-20',
+    blog_link: 'https://sankettambare.substack.com/p/whats-really-worth-the-efforts',
   }
 ];
 
--- src/data/sports.js
+++ src/data/sports.js
@@ -438,6 +438,30 @@ const sportsData = [
         caption: "Slide 5",
       },
     ],
+  },
+  {
+    id: 17,
+    title: "Tata Mumbai Marathon 2026",
+    date: "January 18, 2026",
+    description: "My first full marathon, target 2.55 Hrs!",
+    place: "Mumbai",
+    distance: "42 Kms",
+    time: "05:05:53",
+    timeCertificateLink: "https://sportstimingsolutions.in/results?q=eyJlX25hbWUiOiJUYXRhIE11bWJhaSBNYXJhdGhvbiAyMDI2IiwiZV9pZCI6OTAzMzcsImJpYk5vIjoiOTgxMyJ9",
+    slideImages: [
+      {
+        url: `${PUBLIC_URL}/images/sports/tmm_2026_2.jpeg`,
+        caption: "Slide 1",
+      },
+      {
+        url: `${PUBLIC_URL}/images/sports/tmm_2026_3.jpeg`,
+        caption: "Slide 2",
+      },
+      {
+        url: `${PUBLIC_URL}/images/sports/tmm_2026_1.jpeg`,
+        caption: "Slide 3",
+      }
+    ],
   }
 ];
 
