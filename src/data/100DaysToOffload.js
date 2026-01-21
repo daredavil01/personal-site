bd27b29e99f91f58d6e5ddec7f3990a4afc8a5bb
daredavil01 <sanket.tambare01@gmail.com>
2026-01-21 21:03:41 +0530

    Added blog data

--- src/data/100DaysToOffload.js
+++ src/data/100DaysToOffload.js
@@ -61,6 +61,15 @@ const data = [
     blog_tags: ['100_Days_to_Offload', 'Self-reflection'],
     blog_date: '2026-01-20',
     blog_link: 'https://sankettambare.substack.com/p/whats-really-worth-the-efforts',
+  },
+  {
+    id: 8,
+    blog_title: "Tata Mumbai Marathon 2026",
+    blog_description: "The story of endurance, training and conquering the challenge, tremendously huge one!",
+    challenge_id: '100_days_to_offload',
+    blog_tags: ['100_Days_to_Offload', 'Marathon', 'Running'],
+    blog_date: '2026-01-21',
+    blog_link: 'https://sankettambare.substack.com/p/tmm-2026',
   }
 ];
 
