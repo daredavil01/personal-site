aacbbda38e6b71e872b6276c183eff5c4dc8af38
daredavil01 <sanket.tambare01@gmail.com>
2026-02-13 18:46:44 +0530

    Added new blog data

--- src/data/100DaysToOffload.js
+++ src/data/100DaysToOffload.js
@@ -88,6 +88,15 @@ const data = [
     blog_tags: ['100_Days_to_Offload', "Digital Well-being"],
     blog_date: '2026-01-30',
     blog_link: 'https://sankettambare.substack.com/p/digital-nomad',
+  },
+  {
+    id: 11,
+    blog_title: "The digital self in AI World!",
+    blog_description: "What lies at the end of corridor where our digital identity and physical identity fuse together and start influencing each other, to extend that, there is no difference further!",
+    challenge_id: "100_days_to_offload",
+    blog_tags: ['100_Days_to_Offload', "AI", "Digital Well-being"],
+    blog_date: '2026-02-12',
+    blog_link: 'https://sankettambare.substack.com/p/digital-self-in-ai-world',
   }
 ];
 
