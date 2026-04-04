3774779d028e3a41e168df25d6f6f6701b171b74
daredavil01 <sanket.tambare01@gmail.com>
2026-04-04 11:28:52 +0530

    fixed ui

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-import Navigation from '../components/Template/Navigation';
-import SideBar from '../components/Template/SideBar';
-import Footer from '../components/Template/Footer';
-import ScrollToTop from '../components/Template/ScrollToTop';
-import FloatingToggle from '../components/Template/FloatingToggle';
+import Navigation from "../components/Template/Navigation";
+import SideBar from "../components/Template/SideBar";
+import Footer from "../components/Template/Footer";
+import ScrollToTop from "../components/Template/ScrollToTop";
+import FloatingToggle from "../components/Template/FloatingToggle";
 
 const Main = (props) => {
   return (
@@ -20,17 +20,20 @@ const Main = (props) => {
         {props.title && <title>{props.title}</title>}
         <meta name="description" content={props.description} />
       </Helmet>
-      
+
       <div className="flex flex-col min-h-screen bg-white dark:bg-stone-950 text-stone-900 dark:text-stone-100 font-body transition-colors duration-300">
         <Navigation />
-        
-        <div id="wrapper" className="flex flex-col md:flex-row pt-24 px-4 md:px-8 max-w-[1440px] mx-auto w-full gap-8">
+
+        <div
+          id="wrapper"
+          className="flex flex-col lg:flex-row pt-24 px-4 md:px-8 max-w-[1440px] mx-auto w-full gap-8"
+        >
           <SideBar />
           <main id="main" className="flex-grow py-8 min-h-[50vh]">
             {props.children}
           </main>
         </div>
-        
+
         <Footer />
         <FloatingToggle />
       </div>
